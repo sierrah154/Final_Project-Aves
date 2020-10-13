3a73f08496fa69edd08820a94e43ff1e5b14d462
Catherine Aznoe <44196013+caznoe@users.noreply.github.com>
2020-10-14 00:12:56 +0300

    Adding updated sql schema and join code
    Code includes inner joins on three tables

--- schema.sql
+++ schema.sql
@@ -6,32 +6,54 @@ CREATE TABLE ebirds (
 	locality VARCHAR,
 	state VARCHAR (40),
 	occurence_status VARCHAR,
-	individual_count VARCHAR (10),
-	latitude VARCHAR,
-	longitude VARCHAR,
+	individual_count BIGINT,
+	latitude FLOAT,
+	longitude FLOAT,
 	basis_of_record VARCHAR (40),
 	catalog_number VARCHAR (40),
 	date VARCHAR (15) NOT NULL
 );
-	
+
 -- Creating table for air quality
 CREATE TABLE air_quality(
-	state_name VARCHAR (40),
 	county_name VARCHAR (40),
-	state_code VARCHAR (10),
-	county_code VARCHAR (10),
+	county_code INT,
 	date VARCHAR (10),
-	aqi VARCHAR (10),
+	aqi INT,
 	category VARCHAR (40),
 	defining_parameter VARCHAR (40)
 );
 
 -- Creating table for counties
 CREATE TABLE counties(
-	latitude INT,
-	longitude INT,
-	county VARCHAR (40),
-	PRIMARY KEY (county)
+	latitude FLOAT,
+	longitude FLOAT,
+	county_name VARCHAR (40)
 );
-
+SELECT * FROM ebirds
+SELECT * FROM counties
 SELECT * FROM air_quality
+
+SELECT
+	c.county_name,
+	b.species,
+	b.latitude,
+	b.longitude,
+	b. date
+INTO bird_county
+FROM ebirds b
+INNER JOIN counties c
+ON c.latitude = b.latitude AND c.longitude = b.longitude
+
+SELECT
+	bc.county_name,
+	bc.species,
+	bc.date,
+	a.aqi,
+	a.category
+INTO bird_aqi 
+FROM bird_county bc
+INNER JOIN air_quality a
+ON a.county_name = bc.county_name AND a.date = bc.date
+
+SELECT * FROM bird_aqi
